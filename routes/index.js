09b1a173a1b4f900a59a68019e7149ddb2edc707
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-05-08 17:44:57 -0600

    Render index template with default messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 import { Router } from 'express';
 const router = Router();
 
-/* GET home page. */
+const messages = [
+  {
+    text: 'Hello there!',
+    user: 'Obi-Wan Kenobi',
+    added: new Date('2005-05-19'),
+  },
+  {
+    text: 'General Kenobi!',
+    user: 'General Grievous',
+    added: new Date('2005-05-19'),
+  },
+  {
+    text: 'Wow! This message board is great!',
+    user: 'Roger',
+    added: new Date(Date.now()),
+  },
+];
+
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages: messages });
 });
 
 export default router;
