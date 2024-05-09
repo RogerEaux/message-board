151a5d4a034749528b78b90265d06c6767148d4d
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-05-08 18:03:07 -0600

    Add route to form to add new messages

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,26 @@ const messages = [
   {
     text: 'Hello there!',
     user: 'Obi-Wan Kenobi',
-    added: new Date('2005-05-19'),
+    added: new Date('2005-05-20'),
   },
   {
     text: 'General Kenobi!',
     user: 'General Grievous',
-    added: new Date('2005-05-19'),
+    added: new Date('2005-05-20'),
   },
   {
     text: 'Wow! This message board is great!',
     user: 'Roger',
-    added: new Date(Date.now()),
+    added: new Date('2024-05-09'),
   },
 ];
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Message Board', messages: messages });
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: 'New Message' });
 });
 
 export default router;
