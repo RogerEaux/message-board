9658925efe687e7afb3fdd7e7d4cd4832a3e86b4
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-05-08 18:54:06 -0600

    Remove unnecessary users router

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
 import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
